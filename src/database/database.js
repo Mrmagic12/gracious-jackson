ab19798daafcb1415f475d61b7a2c87c76b53b8c
Mrmagic12 <157940462+Mrmagic12@users.noreply.github.com>
2024-02-22 15:48:50 +0000

    style: Add Model import to Sequelize and DataTypes

--- src/database/database.js
+++ src/database/database.js
@@ -1,33 +1,36 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize, DataTypes, Model } from "sequelize";
 const sequelize = new Sequelize("sqlite::memory:");
 
 class Product extends Model {}
 
-Product.init({
-  product_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
+Product.init(
+  {
+    product_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.REAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    barcode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  product_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.REAL,
-    allowNull: false,
-  },
-  stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  barcode: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  category: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  { sequelize, modelName: "product", timestamps: false },
+);
 
 export { Product };
