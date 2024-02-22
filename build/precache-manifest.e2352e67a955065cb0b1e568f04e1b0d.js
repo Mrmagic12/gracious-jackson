875889c52236f637013f541c00244e6fcf76fa65
Mrmagic12 <157940462+Mrmagic12@users.noreply.github.com>
2024-02-22 13:18:48 +0000

    added

---
+++ build/precache-manifest.e2352e67a955065cb0b1e568f04e1b0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eea146857d1f6ff1f987aae400d27bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ef7e925922637a7fc2",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "f86f7a9b94aa23be7f29",
+    "url": "/static/js/2.91dc03cb.chunk.js"
+  },
+  {
+    "revision": "74ef7e925922637a7fc2",
+    "url": "/static/js/main.ce4197a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
