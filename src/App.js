cb0b996ae40c4b96db89f465ac6347def56b593a
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-02-25 03:59:07 +0000

    feat: Add heading 'Hello' to App component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox v2</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h3>Hello</h3>
     </div>
   );
 }
